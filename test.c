4a81c98e82dee79a5ae9e26f1797cd2d19e93c57
Kun <1433729173@qq.com>
2023-04-06 21:54:24 +0800

    add test file

---
+++ test.c
@@ -0,0 +1,23 @@
+/*********************************************************************************
+ *      Copyright:  (C) 2023 Kun_<1433729173@qq.com>
+ *                  All rights reserved.
+ *
+ *       Filename:  test.c
+ *    Description:  This file for test git
+ *                 
+ *        Version:  1.0.0(2023年04月06日)
+ *         Author:  Kun_ <1433729173@qq.com>
+ *      ChangeLog:  1, Release initial version on "2023年04月06日 21时51分47秒"
+ *                 
+ ********************************************************************************/
+#include <stdio.h>
+ 
+
+int main (int argc, char **argv)
+{
+	
+	printf ("test\n");
+	return 0;
+} 
+
+
