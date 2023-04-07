f464497a86b08d398dddd9afabb2f27ff7557573
Kun <1433729173@qq.com>
2023-04-07 17:03:35 +0800

    clinet.c

---
+++ client.c
@@ -0,0 +1,14 @@
+/*********************************************************************************
+ *      Copyright:  (C) 2023 Kun_<1433729173@qq.com>
+ *                  All rights reserved.
+ *
+ *       Filename:  client.c
+ *    Description:  This file socket client
+ *                 
+ *        Version:  1.0.0(2023年04月07日)
+ *         Author:  Kun_ <1433729173@qq.com>
+ *      ChangeLog:  1, Release initial version on "2023年04月07日 17时00分57秒"
+ *                 
+ ********************************************************************************/
+
+
