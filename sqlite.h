5a2f834ad6e0b6fbfb39901c94e852dc1c8181fe
Kun <1433729173@qq.com>
2023-04-13 17:27:44 +0800

    sqlite

---
+++ sqlite.h
@@ -0,0 +1,24 @@
+/********************************************************************************
+ *      Copyright:  (C) 2023 Kun_<1433729173@qq.com>
+ *                  All rights reserved.
+ *
+ *       Filename:  sqlite.h
+ *    Description:  This file sqlite head file
+ *
+ *        Version:  1.0.0(2023年04月10日)
+ *         Author:  Kun_ <1433729173@qq.com>
+ *      ChangeLog:  1, Release initial version on "2023年04月10日 21时01分23秒"
+ *                 
+ ********************************************************************************/
+#ifndef  _SQLITE_H_
+#define  _SQLITE_H_
+
+#include <stdint.h>
+
+extern int sqlite_init();`
+extern int sqlite_insert(myData data);`
+extern int sqlite_select();`
+extern int sqlite_delete();`
+ 
+#endif
+
