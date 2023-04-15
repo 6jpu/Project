74c472121459148dcb13c50a208a5d5181b890ed
Kun <1433729173@qq.com>
2023-04-15 22:38:42 +0800

    update client

--- ds18b20.c
+++ ds18b20.c
@@ -9,14 +9,15 @@
 #include <dirent.h>
 
 
-int get_temperature(float *temp, char *sn)
+int get_temperature(float *temp)
 {
 	int            fd = -1;
 	char           buf[128];
 	char          *ptr = NULL;
 	DIR           *dirp = NULL;
 	struct dirent *direntp = NULL;
-	char           w1_path[64] = "/sys/bus/w1/devices/";
+	//char           w1_path[64] = "/sys/bus/w1/devices/";
+	char           w1_path[64] = "/home/luofangheng/git/Project/";
 	char           chip_sn[32];
 	int            found = 0;
 
@@ -32,7 +33,6 @@ int get_temperature(float *temp, char *sn)
 		if(strstr(direntp->d_name,"28-"))
 		{
 			strncpy(chip_sn,direntp->d_name,sizeof(chip_sn));
-                        strncpy(sn,chip_sn,sizeof(chip_sn));  //获取设备序列号
 			found = 1;
 		}
 	}
@@ -44,9 +44,8 @@ int get_temperature(float *temp, char *sn)
 
 	closedir(dirp);
 
-        strncat(w1_path, chip_sn, sizeof(w1_path)-strlen(w1_path));
+    strncat(w1_path, chip_sn, sizeof(w1_path)-strlen(w1_path));
 	strncat(w1_path, "/w1_slave", sizeof(w1_path)-strlen(w1_path));
-
 
 	fd=open(w1_path,O_RDONLY);
 	if( fd < 0)
@@ -81,5 +80,44 @@ int get_temperature(float *temp, char *sn)
 	close(fd);
 
         return 0;	
+
+}
+
+
+int get_sn(char *sn)
+{
+    char          *ptr = NULL;
+    DIR           *dirp = NULL;
+    struct dirent *direntp = NULL;
+    //char           w1_path[64] = "/sys/bus/w1/devices/";
+    char           w1_path[64] = "/home/luofangheng/git/Project/";
+    int            found = 0;
+    char           chip_sn[32];
+
+    dirp  = opendir(w1_path);
+    if( !dirp )
+    {   
+        printf("open folder %s failure: %s\n",w1_path,strerror(errno));
+        return -1; 
+    }   
+    while( NULL != (direntp=readdir(dirp)))
+    {   
+        if(strstr(direntp->d_name,"28-"))
+        { 
+			strncpy(chip_sn,direntp->d_name,sizeof(chip_sn));
+			memset(sn,0,sizeof(sn));
+			strncpy(sn,chip_sn,sizeof(chip_sn));
+            found = 1;
+        }   
+    }   
+    if(!found)
+    {   
+        printf("Can not find ds18b20 chipset\n");
+        return -2; 
+    }   
+
+    closedir(dirp);
+    
+    return 0;   
 
 }
