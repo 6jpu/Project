315500bf3995c977f97b1809ad1669d6fa47a8bf
Kun <1433729173@qq.com>
2023-04-30 15:59:13 +0800

    2.0

---
+++ ds18b20/client/lib/get_time.h
@@ -0,0 +1,26 @@
+/********************************************************************************
+ *       Copyright:  (C) 2023 Kun_<1433729173@qq.com>
+ *                  All rights reserved.
+ *
+ *        Filename:  get_time.h
+ *     Description:  This get_time head file
+ *         Version:  2.0.0(2023年04月30日)
+ *          Author:  Kun_ <1433729173@qq.com>
+ *       ChangeLog:  1, Release initial version on "2023年04月15日 10时29分09秒"
+ *                 
+ ********************************************************************************/
+#ifndef  _GET_TIME_H_
+#define  _GET_TIME_H_
+
+
+/* 
+ * 函数介绍：获取当前时间
+ * 输出参数：将时间以YYYY-MM-DD HH:MM:SS 格式的字符串写入repoet_time
+ * 返回值：成功返回0，出错返回负数
+ * */
+int get_time(char *report_time);
+
+ 
+#endif
+
+
---
+++ ds18b20/server/lib/mysqlite.h
@@ -0,0 +1,60 @@
+/********************************************************************************
+ *      Copyright:  (C) 2023 Kun_<1433729173@qq.com>
+ *                  All rights reserved.
+ *
+ *       Filename:  mysqlite.h
+ *    Description:  This file mysqlite head file
+ *
+ *        Version:  1.0.0(2023年04月18日)
+ *         Author:  Kun_ <1433729173@qq.com>
+ *      ChangeLog:  1, Release initial version on "2023年04月10日 21时01分23秒"
+ *                 
+ ********************************************************************************/
+#ifndef  _MYSQLITE_H_
+#define  _MYSQLITE_H_
+
+#define DEVSN_LEN 10
+
+typedef struct packet_s
+{
+	float temp;                /*   temperature */
+	char  time[32];            /*   date/time: YYYY-MM-DD hh:mm:ss */
+	char  devsn[DEVSN_LEN+1];  /*   SN format: RPI2304001 */
+} packet_t; 
+
+
+
+/*
+ * 函数介绍： 创建名为输入参数db_name的数据库并创建名为TEMP的表格
+ * 返回值：正常退出返回0，创建失败返回负数
+ */
+int db_init(char *db_name);
+
+/*
+ * 函数介绍：向TEMP的data插入结构体数据
+ * 输出参数：packet_t类型结构体
+ * 返回值：正常退出返回0，出错返回负数
+ */
+int db_insert(packet_t pack);
+
+/* 
+ * 函数介绍：自动选择表中最后一行数据存入传入参数数组并打印
+ * 输出参数：选择数据存入str中
+ * 返回值：正常退出返回0，出错返回负数
+ */
+int db_select(char *str);
+
+/*
+ * 函数介绍：自动选择表中最后一行数据删除
+ * 返回值：正常退出返回0，出错返回负数
+ */
+int db_delete(void);
+
+/* 
+ * 函数介绍：使用完数据库后关闭数据库
+ * 返回值：正常退出返回0，出错返回负数
+ */
+int db_close(void);
+ 
+#endif
+
